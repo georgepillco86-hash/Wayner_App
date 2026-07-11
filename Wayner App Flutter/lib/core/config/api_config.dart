1f9d61a45bec310b86c950c39182cfc20aa74be1
georgepillco86-hash <georgepillco.86@gmail.com>
2026-07-11 16:56:35 -0500

    Act

--- Wayner App Flutter/lib/core/config/api_config.dart
+++ Wayner App Flutter/lib/core/config/api_config.dart
@@ -1,6 +1,6 @@
 class ApiConfig {
   // Cambia esta IP según donde esté corriendo tu backend.
-  static const String baseUrl = 'http://localhost:8000';
+  static const String baseUrl = 'http://localhost:5000';
   //static const String baseUrl = 'http://10.0.2.2:8000';
   static const String saldosBasePath = '/api/saldos';
 }
